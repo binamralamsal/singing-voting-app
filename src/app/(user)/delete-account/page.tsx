383d6fced90adcc1f70759a30c340db3c2091ce4
binamralamsal <binamralamsal@gmail.com>
2024-06-26 23:57:10 +0545

    asdf

--- src/app/(user)/delete-account/page.tsx
+++ src/app/(user)/delete-account/page.tsx
@@ -1,4 +1,4 @@
-function DeleteAccountForm() {
+export default function DeleteAccountForm() {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-lg">
